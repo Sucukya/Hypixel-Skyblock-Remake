162755cdf1a123f65a4c7edb694fc60e695e27a9
Sucukya <73162173+Sucukya@users.noreply.github.com>
2022-02-26 14:04:58 +0100

    Delete Inspiration directory

--- Inspiration/src/main/java/com/dutchjelly/craftenhance/commandhandling/ICompletionProvider.java
+++
@@ -1,9 +0,0 @@
-package com.dutchjelly.craftenhance.commandhandling;
-
-import java.util.List;
-
-public interface ICompletionProvider {
-
-    List<String> getCompletions(String[] args);
-
-}
--- Inspiration/src/main/java/com/dutchjelly/craftenhance/gui/IButtonHandler.java
+++
@@ -1,8 +0,0 @@
-package com.dutchjelly.craftenhance.gui;
-
-import com.dutchjelly.craftenhance.gui.util.ButtonType;
-import org.bukkit.inventory.ItemStack;
-
-public interface IButtonHandler {
-    void handleClick(ItemStack btn, ButtonType btnType);
-}
